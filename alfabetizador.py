5bc86eafccb06ea179f72b683179b1e3e070f6ad
rogerionepomuceno-lab <rogerio.nepomuceno@alunos.ifsuldeminas.edu.br>
2025-11-13 11:24:37 -0300

    Update alfabetizador.py

--- alfabetizador.py
+++ alfabetizador.py
@@ -28,6 +28,35 @@ if palavra:
     
     for s in silabas:
         grupo = grupo_silaba(s)
+        from hyphen import Hyphenator
+h = Hyphenator('pt_BR')
+
+palavra = 'casa'
+silabas = h.syllables(palavra)
+print(silabas)  # ['ca', 'sa']
+
+import streamlit as st
+from hyphen import Hyphenator
+
+# Lista de consoantes comuns
+consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
+vogais = ['a', 'e', 'i', 'o', 'u']
+
+# Gerar todos os grupos de sílabas C+V
+grupos = [c+v for c in consoantes for v in vogais]
+
+h = Hyphenator('pt_BR')
+
+st.title("Alfabetizador - Método Paulo Freire (Português)")
+palavra = st.text_input("Digite uma palavra:")
+
+if palavra:
+    silabas = h.syllables(palavra)
+    st.write("Sílabas:", silabas)
+    st.write("Grupos de sílabas (C+V):", grupos)
+    grupos_da_palavra = [g for g in grupos if g in silabas]
+    st.write("Grupo de sílabas da palavra:", grupos_da_palavra)
+
         if grupo:
             st.write(f"Sílaba '{s}': grupo {grupo}")
         else:
